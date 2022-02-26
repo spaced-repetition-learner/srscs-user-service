1c6a988850cc7504e3804b5f6e98b1e17c230d49
dadepu <danielkoellgen@gmail.com>
2022-02-26 21:33:02 +0100

    initial

---
+++ src/test/java/de/danielkoellgen/srscsuserservice/SrscsUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.danielkoellgen.srscsuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrscsUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
