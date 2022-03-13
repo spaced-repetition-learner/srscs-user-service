0a79e7bb3e511340a958e9cb132d7ebd84d89675
dadepu <danielkoellgen@gmail.com>
2022-03-13 13:48:47 +0100

    feat(domainprimitive): add mail-address primitive

---
+++ src/main/java/de/danielkoellgen/srscsuserservice/domain/domainprimitives/MailAddress.java
@@ -0,0 +1,31 @@
+package de.danielkoellgen.srscsuserservice.domain.domainprimitives;
+
+import de.danielkoellgen.srscsuserservice.domain.core.AbstractStringValidation;
+import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+@Embeddable
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+public class MailAddress extends AbstractStringValidation {
+
+    public String mailAddress;
+
+    private static final String pattern = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+
+
+    public MailAddress(@NotNull String mailAddress) throws Exception {
+        validateMailAddressOrThrow(mailAddress);
+        this.mailAddress = mailAddress;
+    }
+
+    private void validateMailAddressOrThrow(@NotNull String mailAddress) throws Exception {
+        validateOrThrowRegex(mailAddress, pattern, this::mapToException);
+    }
+
+    private Exception mapToException(String message) {
+        return new MailAddressException(message);
+    }
+}
