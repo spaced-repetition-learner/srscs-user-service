976135ae33511192e2aeb147bef78f8a5ae250fa
dadepu <danielkoellgen@gmail.com>
2022-03-14 14:45:15 +0100

    refactor(user): make general exception more specific

--- src/main/java/de/danielkoellgen/srscsuserservice/domain/user/application/UserService.java
+++ src/main/java/de/danielkoellgen/srscsuserservice/domain/user/application/UserService.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.NoSuchElementException;
 import java.util.UUID;
 
 @Service
@@ -30,7 +31,7 @@ public class UserService {
         return new UserDto(newUser);
     }
 
-    public UserDto disableUser(UUID userId) throws Exception {
+    public UserDto disableUser(UUID userId) throws NoSuchElementException {
         User user = userRepository.findById(userId).orElseThrow();
         user.disableUser();
         userRepository.save(user);
