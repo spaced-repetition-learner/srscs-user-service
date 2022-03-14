1f9be0476ca77044c24acc07db891a6f047949a4
dadepu <danielkoellgen@gmail.com>
2022-03-14 08:05:24 +0100

    tests(users): add full user-service integration test

---
+++ src/test/java/de/danielkoellgen/srscsuserservice/integrationTests/domain/user/application/UserServiceIntegrationTest.java
@@ -0,0 +1,134 @@
+package de.danielkoellgen.srscsuserservice.integrationTests.domain.user.application;
+
+import de.danielkoellgen.srscsuserservice.domain.domainprimitives.MailAddress;
+import de.danielkoellgen.srscsuserservice.domain.domainprimitives.Name;
+import de.danielkoellgen.srscsuserservice.domain.domainprimitives.Username;
+import de.danielkoellgen.srscsuserservice.domain.user.application.UserService;
+import de.danielkoellgen.srscsuserservice.domain.user.domain.User;
+import de.danielkoellgen.srscsuserservice.domain.user.dto.UserDto;
+import de.danielkoellgen.srscsuserservice.domain.user.repository.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+public class UserServiceIntegrationTest {
+
+    private final UserService userService;
+    private final UserRepository userRepository;
+
+    private final UserDto testUser1;
+
+    @Autowired
+    public UserServiceIntegrationTest(UserService userService, UserRepository userRepository) throws Exception {
+        this.userService = userService;
+        this.userRepository = userRepository;
+
+        this.testUser1 = new UserDto(null, new Username("dadepu"), new MailAddress("danielkoellgen@gmail.com"),
+                new Name("Daniel"), new Name("Koellgen"), null);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        this.userRepository.deleteAll();
+    }
+
+    @Test
+    public void shouldAllowToMakeNewUsers() throws Exception {
+        // given
+        UserDto dto = testUser1;
+
+        // when
+        UserDto responseDto = userService.makeUser(dto);
+
+        // then
+        User fetchedUser = userRepository.findById(responseDto.userId).orElseThrow();
+        assertThat(fetchedUser.getUsername())
+                .isEqualTo(dto.username);
+        assertThat(fetchedUser.getMailAddress())
+                .isEqualTo(dto.mailAddress);
+        assertThat(fetchedUser.getFirstName())
+                .isEqualTo(dto.firstName);
+        assertThat(fetchedUser.getLastName())
+                .isEqualTo(dto.lastName);
+        assertThat(fetchedUser.getIsActive())
+                .isTrue();
+    }
+
+    @Test
+    public void shouldFindUserViaUsername() throws Exception {
+        // given
+        UserDto responseDto = userService.makeUser(testUser1);
+
+        // when
+        User fetchedUser = userRepository.findUserByUsername_Username(testUser1.username.username).orElseThrow();
+
+        // then
+        assertThat(fetchedUser.getUserId())
+                .isEqualTo(responseDto.userId);
+    }
+
+    @Test
+    public void shouldFindUserViaMailAddress() throws Exception {
+        // given
+        UserDto responseDto = userService.makeUser(testUser1);
+
+        // when
+        User fetchedUser = userRepository.findUserByMailAddress_MailAddress(testUser1.mailAddress.mailAddress)
+                .orElseThrow();
+
+        // then
+        assertThat(fetchedUser.getUserId())
+                .isEqualTo(responseDto.userId);
+    }
+
+    @Test
+    public void shouldAllowToDisableUsers() throws Exception {
+        // given
+        UserDto responseDto = userService.makeUser(testUser1);
+
+        // when
+        UserDto disabledDto = userService.disableUser(responseDto.userId);
+
+        // then
+        assertThat(disabledDto.isActive)
+                .isFalse();
+
+        // and then
+        User fetchedUser = userRepository.findById(responseDto.userId).orElseThrow();
+        assertThat(fetchedUser.getIsActive())
+                .isFalse();
+    }
+
+    @Test
+    public void shouldPreventDuplicatedUsernames() throws Exception {
+        // given
+        userService.makeUser(testUser1);
+
+        UserDto testUser2 = new UserDto(null, new Username("dadepu"),
+                new MailAddress("anyotheraddress@gmail.com"), new Name("Daniel2"), new Name("Koellgen2"), null);
+
+        // when then
+        assertThrows(Exception.class, () -> {
+            userService.makeUser(testUser2);
+        });
+    }
+
+    @Test
+    public void shouldPreventDuplicatedMailAddresses() throws Exception {
+        // given
+        userService.makeUser(testUser1);
+
+        UserDto testUser2 = new UserDto(null, new Username("anyOtherName"),
+                new MailAddress("danielkoellgen@gmail.com"), new Name("Daniel2"), new Name("Koellgen2"), null);
+
+        // when then
+        assertThrows(Exception.class, () -> {
+            userService.makeUser(testUser2);
+        });
+    }
+}
