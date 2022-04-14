eb61bea3db3ae52532d4dac7eddc449049400cee
dadepu <danielkoellgen@gmail.com>
2022-04-14 14:11:52 +0200

    test: add ApplicationTest

---
+++ src/test/java/de/danielkoellgen/srscsuserservice/integrationTests/SrscsDeckServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package de.danielkoellgen.srscsuserservice.integrationTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+
+@SpringBootTest
+@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:29092", "port=29092"})
+class SrscsDeckServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
